a6d7bfb6fcc5bf166b3767f1ff59cca7cce80c4c
Steffano <smalagadev@gmail.com>
2019-10-07 14:10:58 -0400

    Static files migrated to React files

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    {/*Navigation bar as a header bar */}
+    <nav class="navbar navbar-dark bg-dark">
+      <div class="navbar-brand">Skate Video Guessing Game</div>
+      <div class="navbar-brand">Attempts: <span class="badge badge-pill badge-light" id="attempts"></span></div>
+    </nav>
+
+  {/*Display*/}
+    <div class="card mb-3 container-fluid">
+      <div class="row no-gutters">
+        <div class="col-md-4">
+          <img src="" class="card-img"  id="logo" alt="logo" />
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title card-header text-center" id="game-info">Deletethistext</h5>
+            <div class="card-body" id="blanks"></div>
+            <hr />
+            <small class="card-header container-fluid text-center">Letters used:</small>
+            <small class="card-body" id="guessList"></small>
+          </div>
+        </div>
+      </div>
     </div>
+    </>
   );
 }
 
